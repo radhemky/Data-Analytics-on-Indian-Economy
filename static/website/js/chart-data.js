56ee22c1e92ad672f9ace4fb7e51e874340f01c0
souviksh3 <souviksh3@gmail.com>
2021-05-07 14:03:33 +0530

    changes done

--- static/website/js/chart-data.js
+++ static/website/js/chart-data.js
@@ -1,33 +1,35 @@
-var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
-	
-	var lineChartData = {
-		labels : ["January","February","March","April","May","June","July"],
-		datasets : [
-			{
-				label: "My First dataset",
-				fillColor : "rgba(220,220,220,0.2)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				pointHighlightFill : "#fff",
-				pointHighlightStroke : "rgba(220,220,220,1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-			},
-			{
-				label: "My Second dataset",
-				fillColor : "rgba(48, 164, 255, 0.2)",
-				strokeColor : "rgba(48, 164, 255, 1)",
-				pointColor : "rgba(48, 164, 255, 1)",
-				pointStrokeColor : "#fff",
-				pointHighlightFill : "#fff",
-				pointHighlightStroke : "rgba(48, 164, 255, 1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-			}
-		]
+const randomScalingFactor = function () {
+	return Math.round(Math.random() * 1000)
+};
 
-	}
-		
-	var barChartData = {
+const lineChartData = {
+	labels: ["January", "February", "March", "April", "May", "June", "July"],
+	datasets: [
+		{
+			label: "My First dataset",
+			fillColor: "rgba(220,220,220,0.2)",
+			strokeColor: "rgba(220,220,220,1)",
+			pointColor: "rgba(220,220,220,1)",
+			pointStrokeColor: "#fff",
+			pointHighlightFill: "#fff",
+			pointHighlightStroke: "rgba(220,220,220,1)",
+			data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+		},
+		{
+			label: "My Second dataset",
+			fillColor: "rgba(48, 164, 255, 0.2)",
+			strokeColor: "rgba(48, 164, 255, 1)",
+			pointColor: "rgba(48, 164, 255, 1)",
+			pointStrokeColor: "#fff",
+			pointHighlightFill: "#fff",
+			pointHighlightStroke: "rgba(48, 164, 255, 1)",
+			data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+		}
+	]
+
+};
+
+var barChartData = {
 		labels : ["January","February","March","April","May","June","July"],
 		datasets : [
 			{
@@ -75,7 +77,7 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
 			}
 
 		];
-			
+
 	var doughnutData = [
 				{
 					value: 300,
@@ -103,7 +105,7 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
 				}
 
 			];
-			
+
 	var radarData = {
 	    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
 	    datasets: [
@@ -129,7 +131,7 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
 	        }
 	    ]
 	};
-	
+
 	var polarData = [
 		    {
 		    	value: 300,
@@ -155,6 +157,6 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
 		    	highlight: "#f6495f",
 		    	label: "Red"
 		    }
-		
+
 	];
 
